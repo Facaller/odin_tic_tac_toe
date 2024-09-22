43d762183bc26b7fd7c118511389051b9c64ab4d
Facaller <ashwin.metal13@gmail.com>
2024-09-22 18:04:15 +0200

    Figure out gameController function slowly

--- script.js
+++ script.js
@@ -29,11 +29,28 @@ function createPlayer (player) {
 
     const getPlayer = () => playerSymbol;
 
-    return {player, playerSymbol, getPlayer};
+    return { player, playerSymbol, getPlayer };
 }
 
 function gameController () {
+    let currentPlayer = 'X';
+    const board = gameBoard();
+    let gameOver = false;
 
+    function makeMove (row, column, currentPlayer) {
+        if (gameOver === true) {
+            return
+        }
+
+        const success = board.setCell(row, column, currentPlayer);
+
+        if (!success) {
+            console.log("Cell is filled, try again");
+            return
+        }
+
+        currentPlayer = currentPlayer === 'X' ? 'Y' : 'X';
+    }
 }
 
 function Cell () {
@@ -43,15 +60,15 @@ function Cell () {
         value = symbol
     }
 
-    const isFilled = () => value !== '';
+    const isFilled = () => value !== '' && value !== null;
     const getValue = () => value;
 
-    return {playerSymbol, isFilled, getValue}
+    return { playerSymbol, isFilled, getValue }
 };
 
 const realBoard = gameBoard()
-realBoard.setCell(0, 0, 'X')
-realBoard.setCell(0, 0, 'Y')
+realBoard.setCell(0, 0, 'null')
+realBoard.setCell(0, 1, 'Y')
 
 console.log(realBoard.getBoard()[0][0].getValue())
-console.log(realBoard.getBoard()[0][0].getValue())
+console.log(realBoard.getBoard()[0][1].getValue())
